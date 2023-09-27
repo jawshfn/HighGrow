691e407c4fc7eea002e5a90321377b924f888933
Joshua <nunezjf2001@gmail.com>
2023-09-27 02:22:37 -0400

    Update App.js

--- high-grow/src/App.js
+++ high-grow/src/App.js
@@ -3,26 +3,8 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [currency, setCurrency] = useState(50);
-  const [upgradeLevel, setUpgradeLevel] = useState(0);
-  // Additional state variables and game logic would go here
   
-  const handleUpgrade = () => {
-    // Handle the upgrade logic here
-    // Update the currency and upgradeLevel state variables
-  };
   
-  return (
-    <div className="App">
-      <div className="currency">Currency: {currency}$</div>
-      <Plant 
-        onUpgrade={handleUpgrade}
-        upgradeLevel={upgradeLevel}
-        // Pass other necessary props
-      />
-      {/* Other components and UI elements would go here */}
-    </div>
-  );
 }
 
 export default App;
