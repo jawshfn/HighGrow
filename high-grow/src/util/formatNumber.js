87dcbbd9fa9a8bd2cecd8b40f9f6c41335bb7d5c
Joshua <nunezjf2001@gmail.com>
2023-11-05 15:52:04 -0500

    Updates

--- high-grow/src/util/formatNumber.js
+++
@@ -1,9 +0,0 @@
-// src/utils/formatNumber.js
-export const formatNumber = (num) => {
-    if (num < 1e3) return num.toString(); // Less than 1,000
-    if (num < 1e6) return +(num / 1e3).toFixed(3) + "K"; // Less than 1,000,000
-    if (num < 1e9) return +(num / 1e6).toFixed(1) + "M"; // Less than 1,000,000,000
-    if (num < 1e12) return +(num / 1e9).toFixed(1) + "B"; // Less than 1,000,000,000,000
-    return +(num / 1e12).toFixed(1) + "T"; // Trillion or more
-  };
-  
